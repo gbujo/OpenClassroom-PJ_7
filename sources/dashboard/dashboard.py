8330af98f08ba8587493cc79817b3ac411948932
Guillaume <guillaume.bujosa@gmail.com>
2025-01-29 17:20:01 +0100

    Dashboard 2 graphs (erreur ordre legende)

--- sources/dashboard/dashboard.py
+++ sources/dashboard/dashboard.py
@@ -16,15 +16,13 @@ sys.path.append(chemin_module)
 # Maintenant, vous pouvez importer le module
 import predict_client  # dans répertoire différent
 
-
-st.title('Credit approval dashboard')
-
-
-# Accès aux data
+# Variables globales
 #
-data_path = 'c:/users/innov/python_venv/oc-pj7/input'
+
+
+
 @st.cache_data
-def load_data():
+def load_data(data_path, domain_features):
 #    data = pd.read_csv(f'{data_path}/application_train_20250103.csv', nrows=10000)
     data = pd.read_csv(f'{data_path}/application_train_20250103.csv', nrows=10000)
     data = data.iloc[:, 3:]  # Je supprime des colonnes de réplication des index (je ne sais pas d'où elles viennent mais pas grave)
@@ -34,45 +32,10 @@ def load_data():
     return data, client_base
 
 @st.cache_data
-def load_client(id_client):
-    domain_features=['DAYS_BIRTH', 'CODE_GENDER_F', 'FLAG_RISKED_ORGANIZATION_TYPE', 
-                     'RATIO_INCOME_TO_FAM_MEMBERS', 'FLAG_STABLE_INCOME_TYPE',
-                     'EXT_SOURCE_2',
-                     'AMT_ALL_SUM_MEAN', 'RATIO_ALL_SUM_DEBT_TO_INCOME', 'RATIO_ALL_SUM_OVERDUE_TO_INCOME',
-                     'RATIO_ALL_MAX_OVERDUE_TO_INCOME']
+def load_client(id_client, client_base, domain_features):
 #    client_data = client_base.loc[client_base['SK_ID_CURR'] == id_client, domain_features].stack()
     client_data = client_base.loc[client_base['SK_ID_CURR'] == id_client, domain_features]
     return client_data
-
-
-# Load data
-#
-# Create a text element and let the reader know the data is loading.
-data_load_state = st.text('Loading data...')
-# Load data
-data, client_base = load_data()
-# Notify the reader that the data was successfully loaded.
-data_load_state.text("Done! (using st.cache_data)")
-
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(client_base)
-
-# Client courant
-#
-id_client = st.number_input('Numéro client :', value=None, format="%d", min_value=0, step=1)  # Input ID client
-client_data = load_client(id_client)  # Recherche infos sur le client
-st.subheader('Client data :')
-st.write(client_data.stack())
-
-
-# Prédictions sur le client courant :
-client_decision, client_score, seuil_decision = predict_client.predict_client(client_data.to_numpy().reshape(1, -1))  # Transfo dataframe en array 2 dimensions (10 features, 1 sample)
-client_score = client_score*100
-seuil_decision = seuil_decision*100
-st.subheader(f'Evaluation client : {"Client risqué" if client_decision == 1 else "Client non risqué"}')
-st.write(predict_client.predict_client(client_data.to_numpy().reshape(1, -1)))
-
 
 # Afficher une jauge
 #
@@ -98,21 +61,117 @@ def afficher_jauge(valeur, titre, seuil, min_val=0, max_val=100):
         }
     ))
     st.plotly_chart(fig)
+    return
 
-afficher_jauge(client_score, "Score risque client", seuil_decision, 0, 100)
 
-# Autres clients
+def main():
+    try:
+        st.title('Credit approval dashboard')
+    
+        # Paramètres Features
+        #
+        # Chemin d'accès aux datas
+        data_path = 'c:/users/innov/python_venv/oc-pj7/input'
+
+#        # Liste features
+#        feature_domain=['DAYS_BIRTH', 'CODE_GENDER_F', 'FLAG_RISKED_ORGANIZATION_TYPE', 
+#                 'RATIO_INCOME_TO_FAM_MEMBERS', 'FLAG_STABLE_INCOME_TYPE',
+#                 'EXT_SOURCE_2',
+#                 'AMT_ALL_SUM_MEAN', 'RATIO_ALL_SUM_DEBT_TO_INCOME', 'RATIO_ALL_SUM_OVERDUE_TO_INCOME',
+#                 'RATIO_ALL_MAX_OVERDUE_TO_INCOME']
+
+        # label des valeurs à afficher pour les features qualitatives
+        feature_quali = {'CODE_GENDER_F': ['Autre genre','Genre féminin'],
+                        'FLAG_RISKED_ORGANIZATION_TYPE': ['Autre organisation','Auto-entrepreneur'],
+                        'FLAG_STABLE_INCOME_TYPE': ['Autre', 'Pension et retraite']}
+        # Label à afficher pour les features
+#        feature_label=['Age', 'Genre', 'Organisation professionelle', 'Revenus par membre du foyer', 'Stabilité revenus', 'Evaluation risque externe',
+#                        'Montant moyen crédits passés', 'Ratio d\'endettement', 'Ratio défault de paiement', 'Ratio maximal défaut paiement mensuel']
+        feature_label={'DAYS_BIRTH': 'Age',
+                        'CODE_GENDER_F': 'Genre',
+                        'FLAG_RISKED_ORGANIZATION_TYPE': 'Organisation professionelle', 
+                        'RATIO_INCOME_TO_FAM_MEMBERS': 'Revenus par membre du foyer',
+                        'FLAG_STABLE_INCOME_TYPE': 'Stabilité revenus',
+                        'EXT_SOURCE_2': 'Evaluation risque externe',
+                        'AMT_ALL_SUM_MEAN': 'Montant moyen crédits passés',
+                        'RATIO_ALL_SUM_DEBT_TO_INCOME': 'Ratio d\'endettement',
+                        'RATIO_ALL_SUM_OVERDUE_TO_INCOME': 'Ratio défault de paiement',
+                        'RATIO_ALL_MAX_OVERDUE_TO_INCOME': 'Ratio défaut paiement mensuel MAX'}
+        feature_label_inverse = {valeur: cle for cle, valeur in feature_label.items()}  # Dictionnaire inversé pour retrouver le code avec le libellé
+        feature_domain = list(feature_label.keys())
+
+        # Load data
+        #
+        # Create a text element and let the reader know the data is loading.
+        data_load_state = st.text('Loading data...')
+        # Load data
+        data, client_base = load_data(data_path, feature_domain)
+        # Notify the reader that the data was successfully loaded.
+        data_load_state.text("Done! (using st.cache_data)")
+        
+        if st.checkbox('Show raw data'):
+            st.subheader('Raw data')
+            st.write(client_base)
+        
+        # Client courant
+        #
+        id_client = st.number_input(label='Numéro client :', value=None, format="%d", min_value=0, step=1, placeholder='Renseigner un numéro de client')  # Input ID client
+        if id_client is None :
+            raise ValueError('Renseigner un numéro de client')
+        client_data = load_client(id_client, client_base, feature_domain)  # Recherche infos sur le client
+        if client_data.empty :
+            raise ValueError('Numéro de client inconnu')
+        st.subheader('Client data :')
+        client_data_affichage = client_data.copy()
+        client_data_affichage.columns = list(feature_label.values())
+        st.write(client_data_affichage.stack())
+        
+        
+        # Prédictions sur le client courant 
+        #
+        client_decision, client_score, seuil_decision = predict_client.predict_client(client_data.to_numpy().reshape(1, -1))  # Transfo dataframe en array 2 dimensions (10 features, 1 sample)
+        client_score = client_score*100
+        seuil_decision = seuil_decision*100
+        st.subheader(f'Evaluation client : {"Client risqué" if client_decision == 1 else "Client non risqué"}')
+        st.write(predict_client.predict_client(client_data.to_numpy().reshape(1, -1)))
+        
+        
+        
+        afficher_jauge(client_score, "Score risque client", seuil_decision, 0, 100)
+        
+        # Comparaison avec Autres clients
+        #
+        st.subheader('Positionnement du client :')
+        feature_selected = st.multiselect(label='Selectionner 2 features à afficher :',
+                                           options=list(feature_label.values()),
+                                           max_selections=2,
+                                          placeholder='Sélectionner 2 features à comparer')
+        if len(feature_selected) == 2 : 
+            feature1 = feature_label_inverse[feature_selected[0]]
+            feature2 = feature_label_inverse[feature_selected[1]]
+                         
+            # Affichage du graphique dans Streamlit
+            fig, axs = plt.subplots(1,2, layout='constrained')
+
+            # Choix du graph selon le type de features (quanti ou quali)
+            for i, feat in enumerate([feature1, feature2]) :
+                if feat in feature_quali :  # Feature de type qualitative
+                    _ = dashboard_graphs.graph_2categories(axs[i], data, feat, feature_quali[feat], feature_label[feat],
+                                                             client_target=client_decision, client_data=client_data)
+                else :
+                    _ = dashboard_graphs.graph_qualitatif(axs[i], data, feat, None, feature_label[feat],
+                                                             client_target=client_decision, client_data=client_data)
+            
+            st.pyplot(fig)  # On passe directement l'objet figure
+        else :
+            st.write('Vous devez selectionner 2 features')
+
+    except ValueError as e:
+        st.write(f'Error : {e}')
+    
+    return
+
+# Code principal
 #
-st.subheader('Positionnement du client :')
-
-#  A FAIRE
-# dict_graph = dashboard_graphs.init_graph()
-
-# Affichage du graphique dans Streamlit
-fig, axs = plt.subplots(1,2, layout='constrained')
-_ = dashboard_graphs.graph_2categories(axs[0], data, 'CODE_GENDER_F', ['Autre genre','Genre féminin', 'Client en cours'],
-                                         client_target=client_decision, client_data=client_data)
-_ = dashboard_graphs.graph_2categories(axs[1], data, 'FLAG_RISKED_ORGANIZATION_TYPE', ['Autre organisation','Auto-entrepreneur', 'Client en cours'],
-                                         client_target=client_decision, client_data=client_data)
-
-st.pyplot(fig)  # On passe directement l'objet figure
+if __name__ == "__main__":
+    main()
--- sources/dashboard/dashboard_graphs.py
+++ sources/dashboard/dashboard_graphs.py
@@ -3,60 +3,18 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-def init_graphs():
-# Créer un dictionnaire avec titre et légende à afficher selon la feature
-    dict_labels = {
-        
-    }
-    return dict_labels
-
-def graph_code_gender_f_OLD(axe, data):
-    feature = 'CODE_GENDER_F'
-    mask = data['TARGET'] == 1
-    axe.hist([data.loc[mask, feature], data.loc[~mask, feature]], bins=2, density=True, histtype='bar', 
-            label=['Client risqué', 'Client non risqué'],
-           color=['r', 'g'])
-    axe.set_xlabel('Genre du client')
-    axe.set_ylabel('Proportion des clients')
-    axe.set_title('Donnée : Genre du client')
-    axe.set_xticks([0.25, 0.75], ['Autres', 'Féminin'])
-    axe.legend()    
-    return axe
+import copy
 
 
-def graph_code_gender_f(axe, data):
-    feature = 'CODE_GENDER_F'
-    proportions = data.groupby(['TARGET', feature])[feature].count() / data.groupby('TARGET')[feature].count()
-    proportions = proportions.unstack().to_numpy().transpose()*100
-    
-    feature_label = ['Autre genre','Genre féminin']
-    target_label = ['Client non risqué', 'Cient à risque']
-    
-#    fig, ax = plt.subplots()
-    bottom = np.zeros(2)
-    for ligne in proportions :
-        p = axe.bar([0.25, 0.75], ligne, width=0.25, bottom=bottom)
-        bottom += ligne
-        axe.bar_label(p, label_type='center', fmt='%.1f%%')
-    axe.set_xlabel('Risque client')
-    axe.set_ylabel('Proportion des clients (%)')
-    axe.set_title('Proportion du risque client par genre')
-    axe.set_xticks([0.25, 0.75], target_label)
-    axe.set_xlim([0, 1])
-    axe.legend(feature_label)
-    plt.show()
-    return axe
+def graph_2categories(axe, data, feature, feature_quali, feature_label, client_target, client_data) :
 
-def graph_2categories(axe, data, feature, feature_label, client_target, client_data) :
-    import copy
-    
-#    client_target = 0
-#    client_code_gender_f = 1
-    
-#    feature_label = ['Autre genre','Genre féminin', 'Client en cours']
-    client_feature = client_data[feature].to_list()[0]
     target_label = ['Client non risqué', 'Client à risque']
+    
+    feature_quali.append('Client en cours') # Pour la légende, on ajoute le client en cours
+    client_feature = client_data[feature].to_list()[0]
+
     
     # Proportions des clients selon la target (colonne) et par genre (ligne)
     proportions = data.groupby(['TARGET', feature])[feature].count() / data.groupby('TARGET')[feature].count()
@@ -67,7 +25,6 @@ def graph_2categories(axe, data, feature, feature_label, client_target, client_d
     pos_client[client_feature, client_target] = 1
     pos_client = pos_client * proportions  # On ne garde que la valeur Target et Genre correspondant au client
     
-#    fig, ax = plt.subplots()
     bottom = np.zeros(2)
     pos_client_bottom = np.zeros(2)
     for i, ligne in enumerate(proportions) :
@@ -76,16 +33,54 @@ def graph_2categories(axe, data, feature, feature_label, client_target, client_d
             pos_client_bottom = copy.deepcopy(bottom)  # On conserve la base pour pouvoir afficher la cellule du client après
         bottom += ligne
         axe.bar_label(p, label_type='center', fmt='%.1f%%')
-    #ax.bar([0.25, 0.75], pos_client[client_code_gender_f], width=0.25, bottom=pos_client_bottom,
-    #       color=None, linestyle='--', linewidth=3)
     axe.bar([0.25, 0.75], pos_client[client_feature], width=0.25, bottom=pos_client_bottom,
-           color='none', edgecolor='g', facecolor='none', linestyle='--', linewidth=2.0)
-    axe.set_xlabel('Risque client')
-    axe.set_ylabel('Proportion des clients (%)')
-    axe.set_title('Répartition des risques client')
-    axe.set_xticks([0.25, 0.75], target_label)
+           color='none', edgecolor='b', facecolor='none', linestyle='--', linewidth=2.0)
+
+    axe.set_xlabel('Risque client', fontsize='medium')
+    axe.set_ylabel('Proportion des clients', fontsize='small')
+    axe.set_title(feature_label, fontsize='medium')
+    axe.set_xticks([0.25, 0.75], target_label, fontsize='small')
+    axe.tick_params(axis='both', labelsize='small')
     axe.set_xlim([0, 1])
-    axe.legend(feature_label)
+    axe.legend(feature_quali, fontsize='small')
     plt.show()
     
+    return axe
+
+
+    
+def graph_qualitatif(axe, data, feature, feature_quali, feature_label, client_target, client_data) :
+    # Limite min et max selon les quantiles
+    xmin = data[feature].quantile(0.10)
+    xmax = data[feature].quantile(0.90)
+    
+    target_label = ['Client non risqué', 'Client à risque', 'Client en cours']  # Ajoute client en cours pour la légende
+    
+    # Construction serie 2D : data pour les deux valeurs de TARGET
+#    data_grouped = data.groupby('TARGET')[feature]
+    data_grouped = data.loc[(data[feature] >= xmin) & (data[feature] <= xmax)].groupby('TARGET')[feature]
+    serie=[]
+    for name, group in data_grouped:
+        serie.append(group.to_list())
+
+    # Positionnement du client 
+    client_feature = client_data[feature].to_list()[0]
+
+#    axe.hist(serie, bins=100, density=True, label=target_label, color=['g', 'r'], histtype='step')
+    sns.kdeplot(data, x=feature, hue='TARGET', common_norm=False, ax=axe, palette=['g', 'r'])
+    
+    axe.set_xlabel(feature_label, fontsize='medium')
+    axe.set_ylabel('Proportion des clients', fontsize='small')
+    axe.set_title(feature_label, fontsize='medium')
+    axe.tick_params(axis='both', labelsize='small')
+    if client_feature < xmin : 
+        axe.axvline(xmin, c='b', ls='--')
+    elif client_feature > xmax :
+        axe.axvline(xmax, c='b', ls='--')
+        plt.arrow(0, xmax, 0.5, 0.5)
+    else :
+        axe.axvline(client_feature, c='b', ls='--')
+    axe.legend(target_label, fontsize='small')  
+    plt.show()
+
     return axe
