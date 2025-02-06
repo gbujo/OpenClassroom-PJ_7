1d900757b19c9202d64e867a0dd10bee09304360
Guillaume <guillaume.bujosa@gmail.com>
2025-02-06 23:50:09 +0100

    Dockerfile predict

--- sources/predict/.ipynb_checkpoints/predict_client-checkpoint.py
+++ sources/predict/.ipynb_checkpoints/predict_client-checkpoint.py
@@ -46,7 +46,8 @@ def predict_client(X) :
 
     # Load model
     # Indique le chemin absolu du répertoire contenant le module
-    model_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'model', 'model.pkl')) 
+#    model_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'model', 'model.pkl')) 
+    model_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'model.pkl'))  # Model se trouve dans le meme répertoire
     loaded_model = load_model(model_path)
     
     if loaded_model:
@@ -71,7 +72,8 @@ def predict_Oneclient(X) :
 
     # Load model
     # Indique le chemin absolu du répertoire contenant le module
-    model_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'model', 'model.pkl')) 
+#    model_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'model', 'model.pkl')) 
+    model_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'model.pkl'))  # model dans le meme répertoire (c est plus simple)
     loaded_model = load_model(model_path)
     
     if loaded_model:
