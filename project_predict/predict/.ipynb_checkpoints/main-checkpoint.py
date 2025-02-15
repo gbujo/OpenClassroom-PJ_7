c9e730e3fa6f5d51f539ac007677972d48cff815
Guillaume <guillaume.bujosa@gmail.com>
2025-02-15 10:50:19 +0100

    Correction predict main

--- project_predict/predict/.ipynb_checkpoints/main-checkpoint.py
+++ project_predict/predict/.ipynb_checkpoints/main-checkpoint.py
@@ -4,7 +4,7 @@ from flask import Flask, request, jsonify
 import numpy as np
 
 #import predict.predict_client as predict_client  # Importe un module (module predict_client dans le package predict)
-from predict import predict_client  # j'essaye cette formulation plus simple sans référence au package
+from . import predict_client  # j'essaye cette formulation plus simple sans référence au package
 
 from predict import create_app  # Importe une fonction
 
